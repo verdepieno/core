a30c3b65d64cacb276715c2c3a2e54b7630ed47a
Dmitry Fedyuk <dfediuk@gmail.com>
2019-06-04 16:26:11 +0600

    1.1.3

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -1,5 +1,6 @@
 <?php
 namespace Verdepieno\Core\Setup;
+use Verdepieno\Core\Setup\UpgradeData as D;
 // 2019-03-06
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class UpgradeSchema extends \Df\Framework\Upgrade\Schema {
@@ -11,14 +12,14 @@ class UpgradeSchema extends \Df\Framework\Upgrade\Schema {
 	 */
 	final protected function _process() {
 		if ($this->isInitial()) {
-			$this->columnCAE(UpgradeData::F__CODICE_SDI);
-			$this->columnCAE(UpgradeData::F__PEC);
+			df_dbc_ca(D::F__CODICE_SDI);
+			df_dbc_ca(D::F__PEC);
 		}
 		if ($this->v('0.0.2')) {
-			$this->column('quote_address', UpgradeData::F__CODICE_SDI);
-			$this->column('sales_order_address', UpgradeData::F__CODICE_SDI);
-			$this->column('quote_address', UpgradeData::F__PEC);
-			$this->column('sales_order_address', UpgradeData::F__PEC);
+			df_dbc_oa(D::F__CODICE_SDI);
+			df_dbc_oa(D::F__PEC);
+			df_dbc_qa(D::F__CODICE_SDI);
+			df_dbc_qa(D::F__PEC);
 		}
 	}
 }
